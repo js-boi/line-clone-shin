53b7295d137759918dc0f53683e471513d664e09
tsuboi <jsboi1030@gmail.com>
2022-02-27 13:55:33 +0900

    [add] SignInでボタンを追加

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Button} from "@mui/material";
 
 const SignIn = () => {
+    const signInWithGoogle = () => {
+        console.log("グーグルサインイン")
+    }
+
     return (
         <div>
             <p>SignIn</p>
+            <Button onClick={signInWithGoogle}>グーグルでログインする</Button>
         </div>
     );
 };
