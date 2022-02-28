0f51991a0dbf71bb16929a472d69665edee01338
tsuboi <jsboi1030@gmail.com>
2022-02-28 16:33:24 +0900

    [add] データ送信＋メッセージ表示

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,14 +1,31 @@
 import React, {useState} from 'react';
+import {db, auth} from '../firebase.js';
+import firebase from 'firebase/compat/app';
 
 const SendMessage = () => {
     const [message, setMessage] = useState("");
 
+    const {uid, photoURL} = auth.currentUser;
+
+    function sendMessage(e) {
+        e.preventDefault();
+        db.collection("messages").add({
+            text: message,
+            photoURL,
+            uid,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+        setMessage("");
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={sendMessage}>
                 <div className="sendMsg">
                     <input type="text"
                            placeholder="メッセージを入力してください"
+                           onChange={(e) => setMessage(e.target.value)}
+                           value={message}
                     />
                 </div>
             </form>
