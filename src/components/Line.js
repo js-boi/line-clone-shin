1e95614262102adf064ea6f554c94007bb8d1d39
tsuboi <jsboi1030@gmail.com>
2022-02-28 13:36:04 +0900

    [add] fireStoreの情報をコンソール表示

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SignOut from "./SignOut";
+import {db} from '../firebase';
 
 const Line = () => {
+
+    const [messages, setMessages] = useState();
+
+    useEffect(() => {
+        db.collection("messages")
+            .orderBy("createdAt")
+            .limit(50)
+            .onSnapshot((snapshot) => {
+                setMessages(snapshot.docs.map((doc) => doc.data()));
+            });
+    }, []);
+
     return (
         <div>
-            <SignOut />
+            {console.log(messages)}
+            <SignOut/>
         </div>
     );
 };
