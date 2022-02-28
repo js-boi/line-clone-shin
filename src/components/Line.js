2d989afba2197b9675931044e97af306b3fc8da4
tsuboi <jsboi1030@gmail.com>
2022-02-28 17:02:32 +0900

    [add] 送信側と受信側でCSSを分けて適用

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import SignOut from "./SignOut";
-import {db} from '../firebase';
+import {db, auth} from '../firebase';
 import SendMessage from "./SendMessage";
 
 const Line = () => {
@@ -23,7 +23,7 @@ const Line = () => {
             <div className="msgs">
                 {messages.map(({id, text, photoURL, uid}) => (
                         <div>
-                            <div key={id}>
+                            <div key={id} className={`msg ${uid === auth.currentUser.uid ? "sent" : "received"}`}>
                                 <img src={photoURL} alt=""/>
                                 <p>{text}</p>
                             </div>
